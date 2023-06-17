982ea09a99e58410afabee7b8371d55fc2f45284
yuliya.leskova <yuliya.leskova1@gmail.com>
2023-06-17 22:47:01 +0300

    Прорешаны задачки с семинара

---
+++ Desktop/semc#/Seminars/sem1/Program.cs
@@ -0,0 +1,47 @@
+﻿// System.Console.Write("Введите число квадрат которого вы хотите увидеть ->");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// System.Console.WriteLine($"квадрат числа{num} -> {num*num} ");
+
+//task2
+// System.Console.Write("Введите первое число ->");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите второе число ->");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad=num2*num2;
+// if (num1 == quad)
+// {
+//     System.Console.WriteLine($"Первое число {num1} является квадратом второго числа {num2}");
+
+// }
+// else
+// {
+//  System.Console.WriteLine($"Первое число {num1} не является квадратом второго числа {num2}");   
+// }
+
+
+// написать программу которая принимает число n и выводит последовательность чисел от - n до n
+
+// System.Console.Write("Введите число N ->");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int curent = n * (-1);
+// while (curent <= n)
+// {
+//     System.Console.Write(curent + " ");
+//     curent++;
+// }
+
+// Дается трехзначное число,нам нужно вывести последнюю цифру этого числа
+
+// System.Console.Write("Введите трехзначное число ->");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num >= 100 && num <= 999)
+// {
+//     System.Console.WriteLine($" последняя цифра трехзначного числа {num} ->{num % 10}");
+// }
+// else 
+// {
+//     System.Console.WriteLine("Не верно введено число");
+// }
