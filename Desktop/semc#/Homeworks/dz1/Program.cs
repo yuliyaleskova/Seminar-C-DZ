aa28729d0cc6d357fcfe16027f0ddbfe66205c53
yuliya.leskova <yuliya.leskova1@gmail.com>
2023-06-17 23:15:56 +0300

    решена 1 задачка

---
+++ Desktop/semc#/Homeworks/dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите  число 1 ->");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите  число 2 ->");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    System.Console.WriteLine($"большее число {num1} + меньшее число {num2}");
+}
+else
+{
+    System.Console.WriteLine($"большее число {num2},меньшее число {num1}");
+}
