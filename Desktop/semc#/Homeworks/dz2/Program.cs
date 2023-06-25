e222c88eb2a209112903c1a5332494591e911c3a
yuliya.leskova <yuliya.leskova1@gmail.com>
2023-06-25 10:27:40 +0300

    задача 2 решена

--- Desktop/semc#/Homeworks/dz2/Program.cs
+++ Desktop/semc#/Homeworks/dz2/Program.cs
@@ -2,10 +2,24 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 
 
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
+// Console.Write("Введите число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2){
+//   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+// }
+// else 
+// {
+//   Console.WriteLine("-> третьей цифры нет");
+// }
