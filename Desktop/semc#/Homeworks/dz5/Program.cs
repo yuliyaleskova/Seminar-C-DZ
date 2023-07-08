748e2d666008f5a798cb3554d0706654190bf2d1
yuliya.leskova <yuliya.leskova1@gmail.com>
2023-07-08 05:24:09 +0300

    добавила 36задачу

--- Desktop/semc#/Homeworks/dz5/Program.cs
+++ Desktop/semc#/Homeworks/dz5/Program.cs
@@ -1,4 +1,6 @@
-﻿// Console.WriteLine("Введите размер массива");
+﻿//  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+ // Console.WriteLine("Введите размер массива");
 // int size = Convert.ToInt32(Console.ReadLine());
 // int[] numbers = new int[size];
 // FillArrayRandomNumbers(numbers);
@@ -30,7 +32,9 @@
 //     Console.WriteLine();
 // }
 
-
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 // Console.WriteLine("Введите размер массива");
 // int size = Convert.ToInt32(Console.ReadLine());
 // int[] numbers = new int[size];
@@ -61,47 +65,3 @@
 //     Console.Write("]");
 //     Console.WriteLine();
 // }
-
-
-
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-double[] numbers = new double[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-double min = Int32.MaxValue;
-double max = Int32.MinValue;
-
-for (int z = 0; z < numbers.Length; z++)
-{
-    if (numbers[z] > max)
-        {
-            max = numbers[z];
-        }
-    if (numbers[z] < min)
-        {
-            min = numbers[z];
-        }
-}
-
-Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
-Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
-
-void FillArrayRandomNumbers(double[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
-        }
-}
-void PrintArray(double[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
